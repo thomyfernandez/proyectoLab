2aba1bc5882105f0c172a60f1fb9a4b1dc71539f
Thomas Fernandez <thomasfernandez00@gmail.com>
2022-08-22 19:20:39 -0300

    boostrap 4 + conectar a la vista

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
 import express from "express";
+import { dirname } from "path";
 import config from "./config";
+import LTSroutes from './routes/STLtable.routes';
 
-import LTSroutes from './routes/STLtable.routes'
+const path = require('path');
+const morgan = require('morgan');
+const mssql = require('mssql');
+const myConnection = require('express-myconnection');
 
-const app = express()
+
+const app = express();
 
 //settings
-app.set('port', config.port)
+app.set('port', config.port);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+/*
+app.use(myConnection(mssql, {
+    host: 'localhost',
+    user: 'root',
+    password: '1166163488',
+    port: '3000'
+}))
+*/
+//routes
 
 app.use(LTSroutes)
 
--- src/routes/STLtable.routes.js
+++ src/routes/STLtable.routes.js
@@ -4,7 +4,8 @@ import { getSTL } from "../controllers/STLtable.controller";
 const router = Router()
 
 router.get('/STL', getSTL)
-/*router.post('/STL', postSTL)
+/*
+router.post('/STL', postSTL)
 router.put('/STL', putSTL)
 router.delete('/STL', deleteSTL)
 */
